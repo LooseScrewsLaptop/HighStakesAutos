b6c4cb9482c455ebb8837331ac7c52d3803a7ead
LooseScrews <loosescrewsy@gmail.com>
2024-10-28 19:19:29 -0500

    Movement to go to alliance stake and then move to mogo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,61 @@
 #include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+
+pros::MotorGroup left_motors({-20, -19, -18}); 
+pros::MotorGroup right_motors({11, 12, 13}); 
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+pros::Motor intake(10);
+pros::Motor chain(9);
+pros::adi::DigitalOut mogo('E');
+
+pros::Optical color_sensor(1); 
+pros::adi::DigitalOut wall('D');
+pros::adi::DigitalOut jaw('A');
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_motors, // left motor group
+                              &right_motors, // right motor group
+                              12.5, // 10 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 4" omnis
+                              600, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+pros::Imu imu(17);
+
+lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            nullptr, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
 
 /**
  * A callback function for LLEMU's center button.
@@ -58,7 +115,70 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	// set position to x:0, y:0, heading:0
+  chassis.setPose(0, 0, 0);
+  wall.set_value(true);
+  pros::delay(300);
+  wall.set_value(false);
+  jaw.set_value(true);
+
+  chassis.moveToPoint(-2, -12, 4000, {.forwards = false, .maxSpeed = 40},false);
+
+  chain.move(127);
+  pros::delay(350);
+  chain.move(0);
+  
+  chassis.moveToPoint(-2, -12, 4000, {.forwards = false, .maxSpeed = 40},false);
+  
+
+/*
+    chassis.moveToPoint(17, -29, 4000, {.forwards = false, .maxSpeed = 40},false);
+    //picks up mogo
+    mogo.set_value(true);
+    //goes to tthe four stack of discs
+    chassis.moveToPoint(-1, -40, 4000, {.maxSpeed = 40},true);
+    pros::delay(1000);
+    //Intakes that disc
+    intake.move(-127);
+    //pros::delay(2000);
+    chain.move(127);
+    //pros::delay(2200);
+    //chain.move(0); 
+
+    //moves to grab the other dics
+    chassis.moveToPoint(-5, -29, 4000, {.maxSpeed = 40},false);
+    pros::delay(1000);
+    //drops goal
+    
+
+    //
+    //chassis.moveToPoint(-35, 0, 4000, {.forwards = false, .maxSpeed = 40},false);
+
+    //chassis.moveToPoint(-36, 3, 4000, {.forwards = false, .maxSpeed = 65},false);
+    //mogo.set_value(true);
+    
+    //chassis.turnToPoint(-37, 11, 4000, {.forwards = false, .maxSpeed = 50},false);
+    //chassis.turnToPoint(-34, 11, 10, {.forwards = false, .maxSpeed = 50},false);
+
+    //chassis.setPose(0,0,0);
+
+    //chassis.turnToPoint(0, 0, 100000, {.forwards = false, .maxSpeed = 30},false);
+    //printf("Test");
+    //chain.move(127);
+    //pros::delay(1300);
+   
+
+    chassis.moveToPoint(16, -39, 4000, {.maxSpeed = 65},false);
+    chain.move(0);
+    intake.move(0);
+
+
+    //Do an intake spin and chain spin
+    //chassis.moveToPoint(-38, -33, 4000, {.forwards = false, .maxSpeed = 50},false);
+*/
+	
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -73,22 +193,79 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
+bool piston_on = false; 
+bool wpiston_on = true; 
+bool jawon = true;
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	while (true){
+		int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+		chassis.tank(leftY,rightY);
+
+        pros::c::optical_rgb_s_t rgb = color_sensor.get_rgb();
+        int red = rgb.red;
+        int blue = rgb.blue;
+        color_sensor.set_led_pwm(70);
+        // Adjust thresholds according to your specific sensor readings
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
+          // Toggle pneumatic state
+          jawon = !jawon;
+
+          // Set pneumatic based on state
+          jaw.set_value(jawon);
+        }
+        if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y)) {
+          // Toggle pneumatic state
+          jawon = !jawon;
+
+          // Set pneumatic based on state
+          jaw.set_value(jawon);
+        }
+
+        
 
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+		//CHAIN
+		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) ) {
+		chain.move(127); 
+		intake.move(-127);
+		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)){
+			chain.move(30); 
+			intake.move(-127);
+		}
+		}
+		else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+		chain.move(-127);
+		intake.move(127);
+		}
+		else {
+		chain.move(0);
+		intake.move(0);
+		}
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		//MOGO
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+				// Toggle pneumatic state
+				piston_on = !piston_on;
+
+				// Set pneumatic based on state
+				mogo.set_value(piston_on);
+			}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+				// Toggle pneumatic state
+				piston_on = !piston_on;
+
+				// Set pneumatic based on state
+				mogo.set_value(piston_on);
+			}
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) {
+				// Toggle pneumatic state
+				wpiston_on = !wpiston_on;
+
+				// Set pneumatic based on state
+				wall.set_value(wpiston_on);
+			}
+    pros::delay(20);
 	}
 }
